afedb70b099763f09c6cfb62cdc32473eca798f3
Mark Pavlovski <markpavlovski@gmail.com>
2018-07-30 12:35:36 -0700

    added reactotron config to run on ios device

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,6 +1,12 @@
+import {NativeModules} from 'react-native'
 import Reactotron from 'reactotron-react-native'
 
+let scriptHostname
+const scriptURL = NativeModules.SourceCode.scriptURL
+scriptHostname = scriptURL.split('://')[1].split(':')[0]
+
 Reactotron
-  .configure() // controls connection & communication settings
+  //.configure()
+  .configure({host: scriptHostname}) // use when runnig on ios device
   .useReactNative() // add all built-in react native plugins
   .connect() // let's connect!
